d2c63e16cf2536ad11a18078981ef3175658f585
Iamraygb <jesuisrayb@gmail.com>
2024-11-19 11:24:02 -0500

    javascript 2 practice

---
+++ javascript2.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+
+//i is a loop variable;
+for( let i=1; i<=5; i++){
+    //block of code
+    //line1
+    console.log("Great"); 
+}
+let firstname= "harry potter";
+console.log(firstname.charAt(7));
+
+let grade=['A','B+','B', 'C+', 'C', 'D']
+grade.splice(2,0, "crunchy", 10,0)
+console.log(grade);
